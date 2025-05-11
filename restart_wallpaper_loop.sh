0d450554c7d97b8e182bed2db7aacaa0e0c9ae1e
warte67 <121575921+warte67@users.noreply.github.com>
2025-05-11 12:06:44 -0700

    Update restart_wallpaper_loop.sh

--- restart_wallpaper_loop.sh
+++ restart_wallpaper_loop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Path to the wallpaper loop script
 WALLPAPER_LOOP_SCRIPT="/home/jay/Documents/GitHub/dotfiles/wallpaper_loop.sh"
 
@@ -7,9 +9,11 @@ WALLPAPER_LOOP_SCRIPT="/home/jay/Documents/GitHub/dotfiles/wallpaper_loop.sh"
 LOCK_FILE="/tmp/swaybg_rnd.lock"
 if [ -f "$LOCK_FILE" ]; then
     rm -f "$LOCK_FILE"
-    sleep 1  # Brief pause to ensure the previous loop terminates
+    #sleep 1  # Brief pause to ensure the previous loop terminates
+    #notify-send "Success" "Background Changed."
+else
+    notify-send "Success" "Restarting wallpaper loop."
 fi
 
 # Start a new instance of the wallpaper loop
 /bin/bash "$WALLPAPER_LOOP_SCRIPT" &
-#notify-send "Success" "Wallpaper loop restarted."
